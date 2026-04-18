38fce97838f10b9dc9518cbfb1bec34728153f9f
zkan <zkan.cs@gmail.com>
2026-04-18 10:19:44 +0700

    Generate resource for site

---
+++ app/avo/resources/site.rb
@@ -0,0 +1,17 @@
+class Avo::Resources::Site < Avo::BaseResource
+  # self.includes = []
+  # self.attachments = []
+  # self.search = {
+  #   query: -> { query.ransack(id_eq: q, m: "or").result(distinct: false) }
+  # }
+
+  def fields
+    field :id, as: :id
+    field :company_address, as: :textarea
+    field :company_name, as: :text
+    field :company_tax_id, as: :text
+    field :description, as: :textarea
+    field :is_active, as: :boolean
+    field :name, as: :text
+  end
+end
